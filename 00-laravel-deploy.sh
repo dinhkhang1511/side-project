4905a4f7b3d197d81acd41906f43eb0336a9dbd5
FIREGROUP\khangld1 <khangld1@fireapps.vn>
2023-04-26 16:36:18 +0700

    Update docker file

--- 00-laravel-deploy.sh
+++ 00-laravel-deploy.sh
@@ -3,11 +3,11 @@ echo "Running composer"
 # composer global require hirak/prestissimo
 composer install --no-dev
 
-# echo "Caching config..."
-# php artisan config:cache
+echo "Caching config..."
+php artisan config:cache
 
-# echo "Caching routes..."
-# php artisan route:cache
+echo "Caching routes..."
+php artisan route:cache
 
 # echo "Running migrations..."
 # php artisan migrate --force
--- Dockerfile
+++ Dockerfile
@@ -27,5 +27,6 @@ ENV LOG_CHANNEL stderr
 # Allow composer to run as root
 ENV COMPOSER_ALLOW_SUPERUSER 1
 
+ENTRYPOINT ["/bin/bash", "-c", "echo 'Running setup commands'; ./00-laravel-deploy.sh"]
 CMD ["/start.sh"]
 
